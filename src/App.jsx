e90bef6b1ee4c09963c77932f724798cb46bf899
elfinana <elfinana@naver.com>
2023-05-12 22:03:45 +0900

    Feat: 라우터 연결

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import MainPage from "./pages/MainPage";
+import Header from "./components/Layout/Header";
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+import ProfileEdit from "./pages/ProfileEdit";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Header />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/profileedit/:id" element={<ProfileEdit />} />
+            <Route path="/homePage" element={<HomePage />} />
+            <Route path="/profilePage/:id" element={<ProfilePage />} />
+            <Route path="/searchPage" element={<SearchPage />} />
+            <Route path="/detailPage" element={<DetailPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
