fb185f9561ba065fd7b7490fc8ccbafead000762
Jeong sun woo <gostmzmffl@gmail.com>
2023-05-15 14:44:50 +0900

    docs        : MainFullPage.jsx 추가

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -4,9 +4,9 @@ import { useQuery } from "react-query";
 import { detailTv } from "../api/detail/detailTv";
 
 function DetailPage() {
-  const { error, data } = useQuery("detailmovie", detailMovie);
-  console.log("왜:", data);
-  console.log(error);
+  // const { error, data } = useQuery("detailmovie", detailMovie);
+  // console.log("왜:", data);
+  // console.log(error);
   // console.log({ detailMovie });
 
   return <div>조선아</div>;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import MainFullPage from "../components/main/MainFullPage";
 
 function MainPage() {
   return (
     <>
-      <div>MainPage</div>;
+      <MainFullPage></MainFullPage>
     </>
   );
 }
