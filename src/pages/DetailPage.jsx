b672d1abc2ce4d33ea6a5c66e3405cfc9453a9a1
elfinana <elfinana@naver.com>
2023-05-15 09:55:54 +0900

    Feat: tv상세보기

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -4,10 +4,13 @@ import { useQuery } from "react-query";
 import { detailTv } from "../api/detail/detailTv";
 
 function DetailPage() {
-  const { error, data } = useQuery("detailmovie", detailMovie);
-  console.log("왜:", data);
-  console.log(error);
-  // console.log({ detailMovie });
+  // const { error, data } = useQuery("detailmovie", detailMovie);
+  // console.log("왜:", data);
+  // console.log(error);
+  // // console.log({ detailMovie });
+
+  const { data } = useQuery("detailtv", detailTv);
+  console.log("tv상세조회!:", data);
 
   return <div>조선아</div>;
 }
